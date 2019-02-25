2bb9bfd9bf87d69057239d1857691e618f1b9e3b
jeffgo10 <jgo@entando.com>
2019-02-25 11:01:32 +0800

    loading + gallery state implemented

--- src/ui/pages/ImageGrid.js
+++ src/ui/pages/ImageGrid.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import PageLoader from 'ui/components/loader/PageLoader';
 import withTheme from 'ui/withTheme';
 
 const styles = {
@@ -12,21 +13,39 @@ const styles = {
   },
 };
 
-const ImageGridList = ({ classes, imgs }) => (
-  <div className={classes.root}>
-    <GridList cellHeight={160} cols={3}>
-      {imgs.map(tile => (
-        <GridListTile key={tile.img} cols={tile.cols || 1}>
-          <img src={tile.img} alt={tile.title} />
-        </GridListTile>
-      ))}
-    </GridList>
-  </div>
-);
+class ImageGridList extends Component {
+  componentWillMount() {
+    this.props.onWillMount();
+  }
+  render() {
+    const { classes, loading } = this.props;
+    if (loading) {
+      return (
+        <div className={classes.root}>
+          <PageLoader />
+        </div>
+      );
+    }
+    const { imgs } = this.props;
+    return (
+      <div className={classes.root}>
+        <GridList cellHeight={160} cols={3}>
+          {imgs.map(tile => (
+            <GridListTile key={tile.img} cols={tile.cols || 1}>
+              <img src={tile.img} alt={tile.title} />
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    );
+  }
+}
 
 ImageGridList.propTypes = {
   classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
   imgs: PropTypes.array.isRequired, // eslint-disable-line react/forbid-prop-types
+  loading: PropTypes.bool.isRequired,
+  onWillMount: PropTypes.func.isRequired,
 };
 
 export default withTheme(withStyles(styles)(ImageGridList));
--- src/ui/pages/ImageGridContainer.js
+++ src/ui/pages/ImageGridContainer.js
@@ -1,81 +1,22 @@
 import { connect } from 'react-redux';
 
+import { getGalleryImages } from 'state/gallery/selectors';
+import { isItShowingLoad } from 'state/loading/selectors';
+import { fetchGalleryImages } from 'state/gallery/actions';
+
 import ImageGrid from './ImageGrid';
 
-const tileData = [
-  {
-    img: 'https://material-ui.com/static/images/grid-list/breakfast.jpg',
-    title: 'Breakfast',
-    author: 'jill111',
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/burgers.jpg',
-    title: 'Tasty burger',
-    author: 'director90',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/camera.jpg',
-    title: 'Camera',
-    author: 'Danson67',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/morning.jpg',
-    title: 'Morning',
-    author: 'fancycrave1',
-    featured: true,
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/hats.jpg',
-    title: 'Hats',
-    author: 'Hans',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/honey.jpg',
-    title: 'Honey',
-    author: 'fancycravel',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/vegetables.jpg',
-    title: 'Vegetables',
-    author: 'jill111',
-    cols: 2,
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/plant.jpg',
-    title: 'Water plant',
-    author: 'BkrmadtyaKarki',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/mushroom.jpg',
-    title: 'Mushrooms',
-    author: 'PublicDomainPictures',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/olive.jpg',
-    title: 'Olive oil',
-    author: 'congerdesign',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/star.jpg',
-    title: 'Sea star',
-    cols: 2,
-    author: '821292',
-  },
-  {
-    img: 'https://material-ui.com/static/images/grid-list/bike.jpg',
-    title: 'Bike',
-    author: 'danfador',
-  },
-];
+export const mapStateToProps = state => ({
+  imgs: getGalleryImages(state),
+  loading: isItShowingLoad(state),
+});
 
-export const mapStateToProps = state => ({ // eslint-disable-line
-  imgs: tileData,
+export const mapDispatchToProps = dispatch => ({
+  onWillMount: () => dispatch(fetchGalleryImages()),
 });
 
 // connect the component
-const ImageGridContainer = connect(mapStateToProps, null)(ImageGrid);
+const ImageGridContainer = connect(mapStateToProps, mapDispatchToProps)(ImageGrid);
 
 // export connected component (Container)
 export default ImageGridContainer;
